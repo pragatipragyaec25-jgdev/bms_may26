09f85c77f6f5675e8558960a2740e782a7d2a7e6
pragatipragyaec25-jgdev <pragatipragya.ec25@bmsce.ac.in>
2026-05-13 17:50:16 +0530

    add python file

---
+++ pragati samsung.py
@@ -0,0 +1,13 @@
+#bubble search with optimisation:
+arr = list(map(int,input("Enter numbers separated by space: ").split()))
+n = len(arr)
+# best efficiency 
+for i in range (0,n-1):
+  sorted = True
+  for j in range ( 0,n-1-i):
+    if arr[j] > arr[j+1]:
+      arr[j], arr[j+1] = arr[j+1], arr[j] #swapping
+      sorted = False
+  if sorted== True:
+    break
+print("Sorted array:", arr)
